8b6f1a4048fef1a4af743d728ac2ee46efdcc1fe
cristiangabrielbravo92 <cristiangabrielbravo92@gmail.com>
2024-05-14 23:43:20 -0300

    Se corrigieron estilos de card

--- src/components/partials/PeliculasCard.jsx
+++ src/components/partials/PeliculasCard.jsx
@@ -1,6 +1,8 @@
 
 import Card from 'react-bootstrap/Card';
 
+
+
 import { addToFavorites } from '../../utils/getFavorites';    //  Importamos la funcion creada en el Helper o almacenador de funciones de favoritos
 import { db, auth } from "../../utils/firebaseCredentials";   //  Importamos la instancia del servicio incializado con getAuth y guardado en la constante auth
 import { onAuthStateChanged } from "firebase/auth";           //  Importamos los modulos/funciones a utilizar de Firebase Authentication 
@@ -27,38 +29,11 @@ export const PeliculasCard = ({cardItemData}) => {
 
   return (
 
-    <div className="peliculasCard__container">
+    
 
-        {/* Leyenda que indica que componente es, Esto se BORRA */}
-        {/* <span style={{fontSize: '.8rem', color: 'blue'}}>AZUL: Componente Card de Pelicula</span> */}
-
-        {/* <h3>{cardItemData.title}</h3>
-        
-        <p>Puntuacion: {(cardItemData.vote_average).toFixed(1)}</p>
-         */}
-        {/* {cardItemData.poster_path != null
-            ? <img className="peliculasCard__poster-img" src={`https://image.tmdb.org/t/p/w500${cardItemData.poster_path}`} alt="Imagen Pelicula"/>
-            : <img className="peliculasCard__poster-img" src={`/images/emptyPoster.svg`} alt="Imagen Pelicula"/>
-        }
-        <h3>{cardItemData.title}</h3>
-        
-        <p>Puntuacion: {(cardItemData.vote_average).toFixed(1)}</p> */}
-        
-        {/* variante sin shadow */}
-        {/* <Card bg="dark" key="dark" text="light" style={{ width: '18rem' }} className="mb-2">
-        <Card.Img variant="top" src={cardItemData.poster_path != null
-            ? `https://image.tmdb.org/t/p/w500${cardItemData.poster_path}`
-            : `/images/emptyPoster.svg`} />
-        <Card.Body>
-          <Card.Text>
-            <h3>{cardItemData.title}</h3>
-            <p>Puntuacion: {(cardItemData.vote_average).toFixed(1)}</p>
-          </Card.Text>
-        </Card.Body>
-      </Card>
- */}
-          {/* Variante con color al costado */}
       <Card bg="dark" key="dark">
+        
+        
         <Card.Img variant="top" src={cardItemData.poster_path != null
             ? `https://image.tmdb.org/t/p/w500${cardItemData.poster_path}`
             : `/images/emptyPoster.svg`} />
@@ -66,26 +41,21 @@ export const PeliculasCard = ({cardItemData}) => {
           <Card.Text className='peliculasCard_textoLight'>
             <h3>{cardItemData.title}</h3>
             <p>Puntuacion: {(cardItemData.vote_average).toFixed(1)}</p>
-
-{/* desde aca */}
-            <div style={{position: "absolute", bottom: "0", left: "0"}}>
+          <div className='peliculasCard_textoLight'>
               {isFavorite == true   ?<iconify-icon onClick={(event) => removeFavoriteClickHandler(event, cardItemData.id)} style={{fontSize: "2rem", color: "crimson"}} icon="streamline:heart-solid"></iconify-icon>
                                     :<iconify-icon onClick={(event) => addFavoriteClickHandler(event, cardItemData.id)} style={{fontSize: "2rem"}} icon="streamline:heart"></iconify-icon>
               }
             </div>
-{/* hasta aca */}
-
           </Card.Text>
         </Card.Body>
       </Card> 
 
-
-
-
-
-
-    
-    </div>
   )
 
 }
+
+{/* <div style={{position: "absolute", top: "1%", right: "5%"}} className='peliculasCard_textoLight'>
+              {isFavorite == true   ?<iconify-icon onClick={(event) => removeFavoriteClickHandler(event, cardItemData.id)} style={{fontSize: "2rem", color: "crimson"}} icon="streamline:heart-solid"></iconify-icon>
+                                    :<iconify-icon onClick={(event) => addFavoriteClickHandler(event, cardItemData.id)} style={{fontSize: "2rem"}} icon="streamline:heart"></iconify-icon>
+              }
+            </div> */}
